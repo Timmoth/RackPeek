70ac8f47745b691af9ffbfb39a41a630e14b618f
Tim Jones <t.jones@timmoth.com>
2026-01-28 19:06:05 +0000

    Added empty RackPeek.WebUi blazor server project

---
+++ RackPeek.Web/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using RackPeek.Web.Components;
+
+namespace RackPeek.Web;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        StaticWebAssetsLoader.UseStaticWebAssets(
+            builder.Environment,
+            builder.Configuration
+        );
+        
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+        
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
